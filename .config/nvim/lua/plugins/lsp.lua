36f69c92f6e9e3f4d56497134f442074a908b4bd
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-01-09 21:23:33 +0900

    update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -37,6 +37,33 @@ lsp_installer.on_server_ready(function(server)
   opts.autostart = true
   if server.name == "tsserver" then
     opts.autostart = is_node_repo
+    opts.init_options = require("nvim-lsp-ts-utils").init_options
+    opts.on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      local ts_utils = require("nvim-lsp-ts-utils")
+      ts_utils.setup({
+        debug = false,
+        disable_commands = false,
+        enable_import_on_completion = false,
+        import_all_timeout = 5000,
+        import_all_priorities = {
+          same_file = 1,
+          local_files = 2,
+          buffer_content = 3,
+          buffers = 4,
+        },
+        import_all_scan_buffers = 100,
+        import_all_select_source = false,
+        filter_out_diagnostics_by_severity = {},
+        filter_out_diagnostics_by_code = {},
+        auto_inlay_hints = false,
+        inlay_hints_highlight = "Comment",
+        update_imports_on_move = false,
+        require_confirmation_on_move = false,
+        watch_dir = nil,
+      })
+      ts_utils.setup_client(client)
+    end
   elseif server.name == "eslintls" then
     opts.on_attach = function (client, bufnr)
       client.resolved_capabilities.document_formatting = true
