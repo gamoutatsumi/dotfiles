7a9515c263fb38caacb5097eec61140c30d86576
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-01-28 15:36:26 +0900

    update

---
+++ .config/nvim/tsnip/toml.ts
@@ -0,0 +1,13 @@
+import { Snippet } from "https://deno.land/x/tsnip_vim@v0.2/mod.ts";
+
+export const plugins: Snippet = {
+  name: "plugins",
+  text: "[[plugins]]\nrepo = '${1:repo}'",
+  params: [
+    {
+      name: "repo",
+      type: "single_line",
+    },
+  ],
+  render: ({ repo }) => `[[plugins]]\nrepo='${repo?.text ?? ""}'`,
+};
---
+++ .config/nvim/tsnip/typescriptreact.ts
@@ -0,0 +1,22 @@
+import { Snippet } from "https://deno.land/x/tsnip_vim@v0.2/mod.ts";
+
+export const state: Snippet = {
+  name: "useState",
+  text: "const [${1:state}, set${State}] = useState(${2:default_value})",
+  params: [
+    {
+      name: "state",
+      type: "single_line",
+    },
+    {
+      name: "default_value",
+      type: "single_line",
+    },
+  ],
+  render: ({ state, default_value }) =>
+    `const [${state?.text ?? ""}, set${
+      state != null
+        ? `${state.text?.charAt(0).toUpperCase()}${state.text?.slice(1)}`
+        : ""
+    }] = useState(${default_value?.text ?? ""})`,
+};
