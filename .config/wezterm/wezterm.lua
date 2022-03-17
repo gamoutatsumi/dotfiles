405167bd4950f5a3eaebbbd7bb2c8add350e892b
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-03-17 12:07:41 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,12 +3,18 @@ return {
   default_prog = {"zsh", "--login"},
   set_environment_variables = {
     TERM = "xterm-256color",
-    USE_TMUX = "true",
+    USE_TMUX = "false",
   },
   font_size = 11.5,
   font = wezterm.font_with_fallback({
     "HackGenNerd Console"
   }),
   color_scheme = "nightfly",
-  enable_tab_bar = false,
+  enable_tab_bar = true,
+  unix_domains = {
+    {
+      name = "unix",
+    }
+  },
+  default_gui_startup_args = {"connect", "unix"},
 }
