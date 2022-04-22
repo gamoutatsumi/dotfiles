478aa42a415e31de216f9c5eb3fda2cb310eb5cf
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-04-22 21:17:32 +0900

    update

--- dot_xmonad/xmonad.hs
+++ dot_xmonad/xmonad.hs
@@ -56,6 +56,7 @@ myStartupHook = do
         spawnOnce "picom"
         spawnOnce "light-locker --no-late-locking --lock-on-suspend"
         spawn     "easystroke"
+        spawn     "easyeffects --gapplication-service"
 
 myConfig = desktopConfig
     { borderWidth        = borderwidth
