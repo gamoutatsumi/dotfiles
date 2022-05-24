cfcdff878218f5cd2f3e8bd59ae4f272fa1dfbb8
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-05-24 09:37:34 +0900

    update

---
+++ dot_config/nvim/tsnip/typescript.ts
@@ -0,0 +1,17 @@
+import { Snippet } from "https://deno.land/x/tsnip_vim@v0.4/mod.ts";
+
+const reftypes: Snippet = {
+  name: "referenceTypes",
+  text: '/// <reference types="${1:module}" />',
+  params: [
+    {
+      name: "module",
+      type: "single_line",
+    },
+  ],
+  render: ({ module }) => `/// <reference types="${module?.text ?? ""}" />`
+};
+
+export default {
+  reftypes,
+};
--- dot_config/nvim/tsnip/typescriptreact.ts
+++ dot_config/nvim/tsnip/typescriptreact.ts
@@ -1,4 +1,4 @@
-import { Snippet } from "https://deno.land/x/tsnip_vim@v0.2/mod.ts";
+import { Snippet } from "https://deno.land/x/tsnip_vim@v0.4/mod.ts";
 
 const state: Snippet = {
   name: "useState",
