f29f756130e00aaec48211c6d99cf90fde072480
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-08-05 13:35:33 +0900

    update

--- dot_zsh/defer/completion.zsh
+++ dot_zsh/defer/completion.zsh
@@ -1,2 +1,3 @@
 exists gh && eval "$(gh completion --shell zsh)" && compdef _gh gh
 exists deno && eval "$(deno completions zsh)" && compdef _deno deno
+exists kubectl && eval "$(kubectl completion zsh)" && compdef __start_kubectl kc
