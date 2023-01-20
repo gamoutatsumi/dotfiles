053dadeddf77b804dd6e25dbf44485d721c75387
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-01-20 18:13:27 +0900

    update

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -58,34 +58,9 @@ for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
 	opts.autostart = true
 	if server == "tsserver" then
 		opts.autostart = is_node_repo
-		opts.init_options = require("nvim-lsp-ts-utils").init_options
 		opts.on_attach = function(client, bufnr)
 			on_attach(client, bufnr)
-			local ts_utils = require("nvim-lsp-ts-utils")
 			client.server_capabilities.document_formatting = false
-			ts_utils.setup({
-				debug = false,
-				disable_commands = false,
-				enable_import_on_completion = false,
-				import_all_timeout = 5000,
-				import_all_priorities = {
-					same_file = 1,
-					local_files = 2,
-					buffer_content = 3,
-					buffers = 4,
-				},
-				import_all_scan_buffers = 100,
-				import_all_select_source = false,
-				filter_out_diagnostics_by_severity = {},
-				filter_out_diagnostics_by_code = {},
-				auto_inlay_hints = false,
-				inlay_hints_highlight = "Comment",
-				update_imports_on_move = false,
-				require_confirmation_on_move = false,
-				watch_dir = nil,
-				import_module_specifier = "non-relative",
-			})
-			ts_utils.setup_client(client)
 		end
 	elseif server == "tailwindcss" then
 		opts.root_dir = util.root_pattern("tailwind.config.js")
