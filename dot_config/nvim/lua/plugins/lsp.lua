346a00651c61d68e4f9bfeb89a9d091b6d7dca7c
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-01-22 15:37:33 +0900

    update

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -56,22 +56,7 @@ for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
 	opts.on_attach = on_attach
 	opts.capabilities = capabilities
 	opts.autostart = true
-	if server == "tsserver" then
-		require("typescript").setup({
-			disable_commands = false,
-			debug = false,
-			go_to_source_definition = {
-				fallback = true,
-			},
-			server = {
-				autostart = is_node_repo,
-				on_attach = function(client, bufnr)
-					on_attach(client, bufnr)
-					client.server_capabilities.document_formatting = false
-				end,
-			},
-		})
-	elseif server == "tailwindcss" then
+	if server == "tailwindcss" then
 		opts.root_dir = util.root_pattern("tailwind.config.cjs", "tailwind.config.js", "tailwind.config.ts")
 		opts.autostart = true
 	elseif server == "jsonls" then
@@ -138,55 +123,71 @@ if vim.fn.executable("satysfi-language-server") then
 	require("lspconfig")["satysfi-ls"].setup({
 		on_attach = on_attach,
 		autostart = true,
-		capabilties = capabilities,
+		capabilities = capabilities,
 	})
 end
 
+require("typescript").setup({
+	disable_commands = false,
+	debug = false,
+	go_to_source_definition = {
+		fallback = true,
+	},
+	server = {
+		autostart = is_node_repo,
+		on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
+			client.server_capabilities.document_formatting = false
+		end,
+	},
+})
+
 local null_ls = require("null-ls")
+require("mason-null-ls").setup({
+	ensure_installed = nil,
+	automatic_setup = true,
+})
+
+require("mason-null-ls").setup_handlers({
+	eslint_d = function(source_name, methods)
+		for i = 1, #methods do
+			null_ls.register(null_ls.builtins[methods[i]][source_name].with({
+				extra_filetypes = {
+					"markdownreact",
+					"astro",
+				},
+				condition = function()
+					return is_node_repo
+				end,
+			}))
+		end
+	end,
+	prettierd = function(source_name, methods)
+		for i = 1, #methods do
+			null_ls.register(null_ls.builtins[methods[i]][source_name].with({
+				extra_filetypes = {
+					"astro",
+				},
+				condition = function()
+					return is_node_repo
+				end,
+			}))
+		end
+	end,
+	cspell = function(source_name, methods)
+		for i = 1, #methods do
+			null_ls.register(null_ls.builtins[methods[i]][source_name].with({
+				diagnostics_postprocess = function(diagnostic)
+					diagnostic.severity = vim.diagnostic.severity["WARN"]
+				end,
+				extra_args = { "--config", vim.fn.expand("~/.config/cspell/cspell.yaml") },
+			}))
+		end
+	end,
+})
 null_ls.setup({
 	sources = {
 		require("typescript.extensions.null-ls.code-actions"),
-		null_ls.builtins.formatting.prettierd.with({
-			condition = function()
-				return vim.fn.executable("prettierd") > 0 and is_node_repo
-			end,
-		}),
-		null_ls.builtins.diagnostics.eslint_d.with({
-			filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "markdownreact", "astro" },
-			condition = function()
-				return vim.fn.executable("eslint_d") > 0 and is_node_repo
-			end,
-		}),
-		null_ls.builtins.formatting.eslint_d.with({
-			filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "markdownreact", "astro" },
-			condition = function()
-				return vim.fn.executable("eslint_d") > 0
-			end,
-		}),
-		null_ls.builtins.formatting.goimports.with({
-			condition = function()
-				return vim.fn.executable("goimports") > 0
-			end,
-		}),
-		null_ls.builtins.diagnostics.cspell.with({
-			diagnostics_postprocess = function(diagnostic)
-				diagnostic.severity = vim.diagnostic.severity["WARN"]
-			end,
-			condition = function()
-				return vim.fn.executable("cspell") > 0
-			end,
-			extra_args = { "--config", vim.fn.expand("~/.config/cspell/cspell.yaml") },
-		}),
-		null_ls.builtins.formatting.shfmt.with({
-			condition = function()
-				return vim.fn.executable("shfmt") > 0
-			end,
-		}),
-		null_ls.builtins.formatting.stylua.with({
-			condition = function()
-				return vim.fn.executable("stylua") > 0
-			end,
-		}),
 	},
 	on_attach = on_attach,
 })
