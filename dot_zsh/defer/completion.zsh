58ce7bebfab1213e41a392b4360d4fa7d890fa63
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-05-09 11:56:59 +0900

    update

--- dot_zsh/defer/completion.zsh
+++ dot_zsh/defer/completion.zsh
@@ -5,3 +5,4 @@ exists helm && eval "$(helm completion zsh)"
 exists stern && eval "$(stern --completion zsh)"
 exists aws_completer && complete -C "$(which aws_completer)" aws
 exists kubebuilder && eval "$(kubebuilder completion zsh)" && compdef _kubebuilder kubebuilder
+exists rtx && eval "$(rtx completion zsh)" && compdef _rtx rtx
