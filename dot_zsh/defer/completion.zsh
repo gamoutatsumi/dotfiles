329635418505c287c00ee5432f2c58c9274e1161
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-05-17 22:41:13 +0900

    update

--- dot_zsh/defer/completion.zsh
+++ dot_zsh/defer/completion.zsh
@@ -55,14 +55,5 @@ _get_comp_words_by_ref ()
 fi
 fi
 
-exists gh && eval "$(gh completion --shell zsh)" && compdef _gh gh
-exists deno && eval "$(deno completions zsh)" && compdef _deno deno
-exists kubectl && eval "$(kubectl completion zsh)" && compdef __start_kubectl kc
-exists helm && eval "$(helm completion zsh)"
-exists stern && eval "$(stern --completion zsh)"
 exists aws_completer && complete -C "$(which aws_completer)" aws
-exists kubebuilder && eval "$(kubebuilder completion zsh)" && compdef _kubebuilder kubebuilder
-exists rtx && eval "$(rtx completion zsh)" && compdef _rtx rtx
-exists aqua && eval "$(aqua completion zsh)"
-exists bw && eval "$(bw completion --shell zsh)" && compdef _bw bw
 exists openstack && eval "$(openstack complete |sed 's;local comp="${!i}";local comp="${(P)i}";')"
