e37bd48317fa3f328baab7476bb8335adb5b65ad
gamoutatsumi <tatsumi@gamou.dev>
2023-08-08 13:49:06 +0900

    update

--- dot_config/nvim/dein/nvim_lsp.lua
+++ dot_config/nvim/dein/nvim_lsp.lua
@@ -187,6 +187,14 @@ for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
     }
   elseif server == "efm" then
     opts.autostart = not is_deno_repo
+    opts.init_options = {
+      documentFormatting = true,
+      rangeFormatting = true,
+      hover = true,
+      documentSymbol = true,
+      codeAction = true,
+      completion = true
+    }
   end
   lspconfig[server].setup(opts)
   ::continue::
