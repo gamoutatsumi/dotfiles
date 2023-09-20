154701f2b6e506753448a19d1c01849f8ce09436
gamoutatsumi <tatsumi@gamou.dev>
2023-09-20 14:40:45 +0900

    update

--- dot_config/nvim/dein/noice.lua
+++ dot_config/nvim/dein/noice.lua
@@ -1,8 +1,10 @@
 -- lua_post_source {{{
 require("noice").setup {
-  override = {
-    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-    ["vim.lsp.util.stylize_markdown"] = true,
+  lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+    }
   }
 }
 -- }}}
