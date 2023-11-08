ece2594b115da5a185cbf447fa1afc9ed3b25496
gamoutatsumi <tatsumi@gamou.dev>
2023-11-08 16:12:12 +0900

    update

--- dot_config/nvim/dein/hover.lua
+++ dot_config/nvim/dein/hover.lua
@@ -2,8 +2,8 @@
 require("hover").setup {
   init = function()
     -- Require providers
-    require("hover.providers.lsp")
     require('hover.providers.gh')
+    require("hover.providers.lsp")
     -- require('hover.providers.gh_user')
     -- require('hover.providers.jira')
     -- require('hover.providers.man')
