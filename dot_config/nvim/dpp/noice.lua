ab8c692e23f076e104337c1b31063eda1d79423c
gamoutatsumi <tatsumi@gamou.dev>
2023-11-16 10:31:00 +0900

    update

--- dot_config/nvim/dpp/noice.lua
+++ dot_config/nvim/dpp/noice.lua
@@ -1,6 +1,15 @@
 -- lua_post_source {{{
 require("noice").setup {
   lsp = {
+    progress = {
+      enabled = true,
+      -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+      -- See the section on formatting for more details on how to customize.
+      format = "lsp_progress",
+      format_done = "lsp_progress_done",
+      throttle = 1000 / 30, -- frequency to update lsp progress message
+      view = "mini",
+    },
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
