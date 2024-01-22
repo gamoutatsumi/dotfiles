52a76e88a96a1f02c612777ff4c859e95afe85e1
gamoutatsumi <tatsumi@gamou.dev>
2024-01-22 19:01:14 +0900

    update

--- executable_install.sh
+++ executable_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-REPO_NAME="gamoutatsumi/dotfiles"
+REPO_NAME="https://github.com/gamoutatsumi/dotfiles.git"
 go run github.com/twpayne/chezmoi@latest init --apply $REPO_NAME
 go run github.com/aquaproj/aqua-installer@latest
